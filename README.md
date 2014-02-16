eff3f2212263a509446539218cab217b6149b18a
Tobias Meyer <corecoder@gmail.com>
2014-02-16 18:39:37 +0100

    Wrote app and added sample docs

---
+++ README.md
@@ -0,0 +1,15 @@
+# Swagger Doc Server
+Server for Swagger Doc Files
+## Install
+```bash
+$ npm install swagger_doc_server -g
+```
+Create a doc directory like the samples directory
+samples/
+├── pet.json
+├── resource.json
+├── store.json
+└── user.json
+## Usage
+```bash
+$ swaggerdocserver --port <port(Default: 8080)> --dir <dir(absolute or relative; Default: current directory)>
---
+++ index.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var fs = require('fs');
+// CLI interface
+var commander = require('commander');
+
+commander
+	.version(require('./package.json').version)
+	.option('-p, --port <port>', 'WebServer port', 8080)
+	.option('-d, --dir <path>', 'Directory containing doc files', __dirname)
+	.parse(process.argv);
+// Parsing options
+commander.dir = path.resolve(commander.dir);
+// Fetching resources
+var src = {};
+fs.readdirSync(commander.dir).forEach(function (elem) {
+	if (elem.match("^.*\.(json)$") !== null) {
+		src[elem.split('.json')[0]] = JSON.parse(fs.readFileSync(commander.dir + '/' + elem, {encoding: 'utf-8'}));
+		console.log('Added ' + elem);
+	}
+});
+
+
+// WebServer
+var express = require('express');
+var app = express();
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
+
+app.get('/', function(req, res) {
+	res.json(src['resource']);
+});
+
+app.get('/:path', function(req, res) {
+	res.json(src[req.params.path]);
+});
+
+app.listen(commander.port);
+console.log('URL: ' + 'http://localhost:' + commander.port);
+console.log('Doc Directory: ' + commander.dir)
